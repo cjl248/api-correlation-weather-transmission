64adc7c0d87cf5a7dc519408edb6a28b44063286
github cjl248 <github cjl248@cornell.edu>
2020-04-17 17:26:09 -0400

    formated data for mass assignment

--- api/coronavirus_data_api.rb
+++ api/coronavirus_data_api.rb
@@ -5,11 +5,44 @@ require 'json'
 
 URL = 'https://coronavirus-tracker-api.herokuapp.com/v2/locations/225'
 
+# Class for parsing coronavirus confirmed, death data
 class GetCoronavirusData
 
-  def get_coronavirus_data
+  def data
     response = RestClient.get(URL)
     response.body
+  end
+
+  def parse_data
+    data = JSON.parse(self.data)
+    parsed_data = data['location']['timelines']
+    parsed_data
+  end
+
+  def date_data
+    date_array = []
+    dates.map do |date|
+      date_array << {
+        date: date,
+        new_cases: confirmed[date],
+        new_deaths: deaths[date]
+      }
+    end
+    date_array
+  end
+
+  private
+
+  def dates
+    parse_data['confirmed']['timeline'].keys
+  end
+
+  def confirmed
+    parse_data['confirmed']['timeline']
+  end
+
+  def deaths
+    parse_data['deaths']['timeline']
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# Require API Classes
+require_relative '../api/coronavirus_data_api.rb'
+
 # Load the Rails application.
 require_relative 'application'
 
